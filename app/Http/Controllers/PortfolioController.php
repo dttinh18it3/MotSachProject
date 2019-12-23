09042c28a3414adad1df7da8726cbee656e91b22
dotantinh <dttinh.18it3@sict.udn.vn>
2019-12-23 08:01:38 +0700

    add model

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,4 +9,9 @@ class PortfolioController extends Controller
     public function getPortfolio() {
         return view('admin/portfolio');
     }
+
+    public function selectPortfolio() {
+        $portfolio = DB::select('SELECT * FROM tblportfolio');
+    }
+
 }
